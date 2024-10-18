4c9ca67f86e9f69a2e18d827eb311347ab7b0585
Ruhifarhath <ruhifarhath123@gmail.com>
2024-10-18 20:27:30 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,81 @@
+
+
+// var arr=[1,2,4,2,3];
+
+// //forEach 
+// arr.forEach(function(val){
+//     console.log(val+" hi")
+// })
+
+// console.log(arr)
+
+// //map 
+
+// var ans= arr.map(function(val){
+//     return val+"hi";
+// })
+// console.log(ans)
+
+// // filter
+// //var arr=[1,2,4,2,3];
+// var res= arr.filter(function(val){
+//     if(val>2){ return true}
+//     else { return false}
+// })
+
+// console.log(res)
+
+// //find
+// var ans1= arr.find(function(val){
+//     if(val==2) return val;
+// })
+
+// console.log(ans1)
+
+// //indexOf
+
+// var arr2=["ruhi", "razi", "ammi", "abbu"]
+
+// console.log(arr2.indexOf("abbu")) // 3
+
+
+//TOPIC 2 OBJECTS
+ 
+var obj={
+    name: "tv",
+    price:34500,
+    model: 2019
+}
+
+
+Object.freeze(obj)
+obj.price=100
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
