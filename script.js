f917ab77b0076d43f63e25e5483ac9b4f67e6596
Ruhifarhath <ruhifarhath123@gmail.com>
2024-10-20 15:13:34 +0530

    part 2 complete

--- script.js
+++ script.js
@@ -1,6 +1,38 @@
 const fs = require('fs');
 
-fs.writeFile("intro.txt", "Hi you have successfully created adn added data to a file", function(err){
-        if(err) console.error(err);
-        else console.log("done")
-    })
+// fs.appendFile("intro.txt", "Append file appends data to already existing file`", function(err){
+//         if(err) console.error(err);
+//         else console.log("done")
+//     })
+
+// fs.rename("intro.txt","newName.txt",function(err){
+//     if(err) console.error(err);
+//     else console.log("done")
+// })
+
+
+//FOR COPY FILE OPERATION WE ARE CREATING A COPY FOLDER AND TELLING TO CREATE A COPIED FILE IN THAT FOLDER
+
+// fs.copyFile("newName.txt","./copy1/copied.txt",function(err){
+//     if(err) console.error(err);
+//     else console.log("done")
+// })
+
+
+// unlink  USED TO DELETE FILE
+//  fs.unlink("./copy/copied.txt",function(err){
+//     if(err) console.error(err);
+//     else console.log("done")
+//  }) 
+
+
+
+// HTTP
+
+const http = require('http');
+
+const server= http.createServer(function(req, res){
+   res.end("Ruhi U have successfully created a server! ")
+})
+
+server.listen(3000) // to run use node script.js and then in browser type localhost:3000
