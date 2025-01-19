abc226af7e7bf0c6f4bd4855a16cb9c71922bdc3
Ruhifarhath <ruhifarhath123@gmail.com>
2025-01-19 12:46:29 +0530

    list

--- Test.java
+++ Test.java
@@ -4,7 +4,17 @@ import java.util.List;
 public class Test {
 
 public static void main(String[] args) {
+    //Wrapper list
     List<Integer> arr = new ArrayList<>();
+    //primitive list 
+    int[] arr1= {121,34,343, 3};
+
+    for(int i=0;i<arr1.length;i++){
+        System.out.print(arr1[i] + " ");
+
+
+    }
+
 }
    
 
